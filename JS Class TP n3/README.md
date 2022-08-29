6e2dd138d23af043ffc533dcd6e180371592e2da
Nahuel González <88565185+nahuegonzza@users.noreply.github.com>
2022-08-29 01:26:41 -0300

    Add files via upload

---
+++ JS Class TP n3/README.md
@@ -0,0 +1,9 @@
+# Web Dinamico & Diseño Movil, Pablo Abdala, 7mo 2da
+Nota: Algunos datos no fueron utilizados para el calculo final, ya que no estaban dentro de la formula entregada, entre ellos:
+. Hijos por Guarderia
+. Km por Movilidad Propia
+(Ambos no estaban en la formula otorgada)
+. Titulo de Grado
+. Adicionales
+. Objetivo
+(No se poseian datos concretos al respecto)
--- JS Class TP n3/functions.js
+++ JS Class TP n3/functions.js
@@ -34,27 +34,81 @@ class CEmpleado{
         this.obraSocial = obraSocial;
         this.adicional = adicional;
         //------------------------------
+        //this.sueldoBasico = sueldoBasico;
+    }
+    CSueldoBasico(){
+        let a=0;
+        let p1='', p2='', p3='';
+        for(let i=0; i<this.categoria.length; i++){
+            if(this.categoria[i]!='|')
+                if(a==0)
+                    p1=p1+this.categoria[i];
+                else if(a==1)
+                    p2=p2+this.categoria[i];
+                else if(a==2)
+                    p3=p3+this.categoria[i];
+                else
+                    i=10000;
+            else
+                a++;
+        }
+        p1 = p1.trimEnd(); p2 = p2.trimEnd(); p3 = p3.trimEnd();
+        //------------------------------------------------------
+        let sueldoBasico=0;
+        if(p1 = 'B-2')
+            if(p3 = 'Junior') sueldoBasico = 91181,57;
+            else if(p3 = 'Semi-Senior') sueldoBasico =  99508,87;
+            else if(p3 = 'Senior') sueldoBasico = 107836,17;
+        else if(p1 = 'B-10')
+            if(p3 = 'Junior') sueldoBasico = 86522,94;
+            else if(p3 = 'Semi-Senior') sueldoBasico = 94384,37;
+            else if(p3 = 'Senior') sueldoBasico =  102245,81;
+        else if(p1 = 'B-11')
+            if(p3 = 'Junior') sueldoBasico =  84892,41;
+            else if(p3 = 'Semi-Senior') sueldoBasico = 92590,80;
+            else if(p3 = 'Senior') sueldoBasico = 100289,19;
+        else{ } //...
+
+        return sueldoBasico;
     }
     CSueldoBruto(){
-        /* Sueldo Bruto = 
-        Sueldo básico + Antiguedad + IdiomaExtranjero + Titulo de Grado + Presentismo + 
-        Funcion (coordinar... etc)+ Adicionales * Objetivos + Adicionales Voluntarios. */
-        let sueldoBruto=0;
-        let sueldoBasico, sueldoAntiguedad, sueldoTitulo, sueldoPresentismo, sueldoFuncion, sueldoAdicional, sueldoObjetivos;
-        sueldoBruto = sueldoBasico + sueldoAntiguedad + sueldoTitulo + sueldoPresentismo + sueldoFuncion + (sueldoAdicional * sueldoObjetivos) + this.adicional;
+        let sueldoBruto = 0;
+        let sueldoBasico = 0, sueldoAntiguedad = 0, sueldoIdioma = 0, sueldoTitulo = 0
+        let sueldoPresentismo = 0, sueldoFuncion = 0, sueldoObjetivos = 0, sueldoAdicionales = 0
+        //--Sueldo Basico------------------------------
+        sueldoBasico = this.CSueldoBasico();
+        //--Antiguedad------------------------------
+        sueldoAntiguedad = sueldoBasico / 100 * this.antiguedad;
+        //--Idiomas------------------------------
+        if (this.idioma != 'Español') sueldoIdioma = sueldoBasico/100*8;
+        //--Titulo de Grado------------------------------
+
+        //--Presentismo------------------------------
+        if(this.presentismo = true) sueldoIdioma = sueldoBasico/100*2;
+        //--Funcion------------------------------
+        if(this.funcion = 'Coordinador Grupo') sueldoFuncion = sueldoBasico/100*10;
+        else if(this.funcion = 'Lider de Equipo') sueldoFuncion = sueldoBasico/100*20;
+        //--Adicionales------------------------------
+
+        //--Objetivos------------------------------
+
+        //--Calculo Final----------------------------------------------------------------------------------
+        sueldoBruto = sueldoBasico + sueldoAntiguedad + sueldoIdioma + sueldoTitulo + sueldoPresentismo + sueldoFuncion + sueldoAdicionales * sueldoObjetivos + this.adicional;
+        console.log(sueldoBruto);
         return sueldoBruto;
+        //--------------------------------
     }
     CRetenciones(){
         let retencionTotal=0;
         if(this.jubilacion==true)
-            retencionTotal=retencionTotal + (this.sueldoBasico/100*11);
+            retencionTotal=retencionTotal + (this.CSueldoBasico()/100*11);
         if(this.obraSocial==true)
-            retencionTotal=retencionTotal + (this.sueldoBasico/100*3);
+            retencionTotal=retencionTotal + (this.CSueldoBasico()/100*3);
         if(this.sindicato==true)
-            retencionTotal=retencionTotal + (this.sueldoBasico/100*1);
+            retencionTotal=retencionTotal + (this.CSueldoBasico()/100*1);
         return retencionTotal;
     }
-    CSueldoNeto(){ return this.CsueldoBruto() - this.CRetenciones(); }
+    CSueldoNeto(){ return this.CSueldoBruto() - this.CRetenciones() }
 }
 
 let Empleados = [];
@@ -76,7 +130,7 @@ function CrearEmpleado(nombreC, categoria, antiguedad, idioma, hijosG, presentis
         '</label></td> <td><label>' + TrueFalse(Empleados[i].jubilacion) + 
         '</label></td> <td><label>' + TrueFalse(Empleados[i].obraSocial) + 
         '</label></td> <td><label>' + '$' + Empleados[i].adicional + 
-        '</label></td> <td><label>' + '$' + Empleados[i].CRetenciones() + 
+        '</label></td> <td><label>' + '$' + Empleados[i].CSueldoNeto().toFixed(2) + 
         '</label></td> </tr> '
         i++;
     })
